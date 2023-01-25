13ad4f055a9c4d1ac2dcb157bb54fae4ee3139a7
Navdeshsinghsaini <naveshsinghsaini@gmail.com>
2023-01-25 08:38:12 +0530

    solution2

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
 
+let input = document.getElementById('newTodoInput');
+let button = document.getElementById('addTodoBtn');
+
+button.addEventListener("click", validate);
+
+function validate (e) {
+    let data = input.value ;  
+    data = data.trim()  
+  
+    if (data == ""){
+     alert('Empty data not allowed...!')
+    
+    } else {
+        let space = document.createElement ('li')
+        space.innerHTML = data ;
+        document.body.append(space)
+       
+    }
+    
+}
