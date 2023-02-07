e6654739e87a03c54f62128be9e7215349b41266
Navdesh Singh Saini <120104452+Navdeshsinghsaini@users.noreply.github.com>
2023-02-07 21:33:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function validate (e) {
         let space = document.createElement ('li')
         space.innerHTML = data ;
         document.body.append(space)
+		data.innnerHTML = "";
        
     }
     
